35956fc25e8f1d04124977c992a33fe90e84ea6d
rlatmdgk1203 <A20213204@HackerLogin.8.8.8.8>
2024-03-25 20:45:13 +0900

    1003

---
+++ baekjoon/1003/1003.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) {
+        printf("0");
+        return 0;
+    } else if (n == 1) {
+        printf("1");
+        return 1;
+    } else {
+        return fibonacci(n‐1) + fibonacci(n‐2);
+    }
+}Fibonacci;
+
+int main(void){
+    int T, i;
+    Fibonacci N;
+    
+    for (i=1; i<=T; i++){
+        scanf("%d", &N);
+    }
+    
+}
